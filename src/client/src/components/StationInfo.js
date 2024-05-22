e424de74d560ac002d669bb82b417fc1c065a353
jasasuster <92645266+jasasuster@users.noreply.github.com>
2024-05-22 20:18:35 +0200

    Client fetch url change

--- src/client/src/components/StationInfo.js
+++ src/client/src/components/StationInfo.js
@@ -34,7 +34,7 @@ function StationInfo() {
         setIsLoadingPredictions(false);
       } else {
         setIsLoadingPredictions(true);
-        fetch(`http://${process.env.REACT_APP_SERVE_URL}:3000/mbajk/predict`, {
+        fetch(`https://${process.env.REACT_APP_SERVE_URL}:3000/mbajk/predict`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
